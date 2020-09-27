96ebf60a80554e0c0d1336c945f9e1071d4bfe27
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-09-27 11:10:48 -0500

    first commit- strapi init

---
+++ strapi-blog/config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.ctnik.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-blog'),
+        username: env('DATABASE_USERNAME', 'will'),
+        password: env('DATABASE_PASSWORD', 'HLDhqU0fRlYeDZWn'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ strapi-blog/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '002125231bbffb60083a73e09734a7b4'),
+    },
+  },
+});
